b66cb85b1832b40bd128c1d2cde2135bb7dddc85
MAC ARTHUR <delannoyarthur@outlook.com>
2024-04-20 12:31:29 +0200

    may the cdataframe begin

---
+++ cdataframe.c
@@ -0,0 +1,9 @@
+//
+// Created by MAC ARTHUR on 20/04/2024.
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cdataframe.h"
---
+++ cdataframe.h
@@ -0,0 +1,13 @@
+//
+// Created by MAC ARTHUR on 20/04/2024.
+//
+
+#ifndef C_PROJECT_CDATAFRAME_H
+#define C_PROJECT_CDATAFRAME_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#endif //C_PROJECT_CDATAFRAME_H
